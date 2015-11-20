e6e6e1022ddd39d10ab4348988a10a6dc15373cc
ammarm08 <ammar.mian08@gmail.com>
2015-11-19 16:07:36 -0800

    yelp api on server side

--- server/server.js
+++ server/server.js
@@ -5,7 +5,43 @@ var port = process.env.PORT || 8000;
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+});
+
 app.use(express.static(__dirname + '/../client'));
+
+var yelp = require("node-yelp");
+var keys = require("../keys.js");
+
+var yelpClient = yelp.createClient({
+  oauth: {
+    consumer_key: keys.yelp.consumer_key,
+    consumer_secret: keys.yelp.consumer_secret,
+    token: keys.yelp.token,
+    token_secret: keys.yelp.token_secret
+  }
+});
+
+app.get('/api/yelp', function(req, res, next) {
+
+  var options = {};
+  options.term = req.body.term;
+  options.ll = req.body.location.join(", ");
+  options.is_closed = "false";
+
+  yelpClient.search(options)
+  .then(function(results) {
+    res.status(200).json({results: results});
+  })
+  .catch(function(err) {
+    res.status(400).send(err);
+  })
+
+})
 
 server.listen(port);
 
